2564dd28ae1c61bbb311e4f6a42acdc142727c9c
Rahul Kumar <172767765+Rahulkr3201@users.noreply.github.com>
2026-01-12 15:16:39 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -26,10 +26,11 @@ const Footer = () => {
                 </div>
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-                Copyright {new Date().getFullYear()} © GreatStack.dev All Right Reserved.
+                Copyright {new Date().getFullYear()}  All Right Reserved.
             </p>
         </div>
     );
 };
+
 
 export default Footer
